e8a75020ec20358c6fde1ec8ad8247e4f9254574
Guillermo Segura <gsegura.s@hotmail.com>
2021-03-04 21:40:33 -0300

    Commit Inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'customers',
+    loadChildren: () => import('./components/customers/customers.module').then(m => m.CustomersModule) 
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('./components/orders/orders.module').then(m => m.OrdersModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
